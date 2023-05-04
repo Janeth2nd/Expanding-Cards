501467002a067540df028efad64325236867b8a8
Janeth II <janeth_als@hotmail.com>
2023-05-03 20:35:34 -0600

    functions and styles

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const panels = document.querySelectorAll(".panel");
+const panels = document.querySelectorAll(".panel"); //El método querySelectorAll() de un Element devuelve una NodeList representa una lista de 
+                                                     //elementos del documento que coinciden con el grupo de selectores indicados.
 
 panels.forEach(panel => {
-    panel.addEventListener("click", () => {
+    panel.addEventListener("click", () => {    //addEventListener() Registra un evento a un objeto en específico. 
         removeActiveClasses()
-        panel.classList.add('active')
-    })
+        panel.classList.add('active')  //Usar classList es una forma práctica de acceder a la lista de clases de un elemento como una->
+                                       //cadena de texto delimitada por espacios a través de element.className.
+    })                                 //The add() method inserts a new element with a specified value in to a Set object.
 })
 
-function removeActiveClasses() {
+const removeActiveClasses = function () {
     panels.forEach(panel => {
-        panel.classList.remove('active')
+        panel.classList.remove("active") //The Element.remove() method removes the element from the DOM.
     })
 }
+
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
