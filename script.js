93d094a6a030f309393842b6e292b647ffa3bf72
Janeth II <janeth_als@hotmail.com>
2023-05-04 19:13:03 -0600

    removing comments

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const panels = document.querySelectorAll(".panel"); //El método querySelectorAll() de un Element devuelve una NodeList representa una lista de 
-                                                     //elementos del documento que coinciden con el grupo de selectores indicados.
+const panels = document.querySelectorAll(".panel");
 
 panels.forEach(panel => {
-    panel.addEventListener("click", () => {    //addEventListener() Registra un evento a un objeto en específico. 
+    panel.addEventListener("click", () => {
         removeActiveClasses()
-        panel.classList.add('active')  //Usar classList es una forma práctica de acceder a la lista de clases de un elemento como una->
-                                       //cadena de texto delimitada por espacios a través de element.className.
-    })                                 //The add() method inserts a new element with a specified value in to a Set object.
+        panel.classList.add('active')
+    })
 })
 
 const removeActiveClasses = function () {
     panels.forEach(panel => {
-        panel.classList.remove("active") //The Element.remove() method removes the element from the DOM.
+        panel.classList.remove("active")
     })
 }
 
